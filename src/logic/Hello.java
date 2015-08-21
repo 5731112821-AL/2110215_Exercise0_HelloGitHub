f1cbf6bb895bd998fe0800907085205392894c19
5731112821-AL <bobby_nh@me.com>
2015-08-21 11:17:11 +0700

    Add swap check

--- src/logic/Hello.java
+++ src/logic/Hello.java
@@ -21,13 +21,17 @@ public class Hello {
 	public static int[] bubbleSort(int[] array){
 		// TODO Implement this method to sort the given array in an ascending order and return the sorted array.
 		for(int c=0; c<array.length; c++){
+			boolean didSwap = false;
 			for(int c2=1; c2<array.length-c; c2++){
 				if(array[c2-1]>array[c2]){
 					int temp = array[c2-1];
 					array[c2-1] = array[c2];
 					array[c2] = temp;
+					didSwap = true;
 				}
 			}
+			if(!didSwap)
+				break;
 		}
 		return array;
 	}
